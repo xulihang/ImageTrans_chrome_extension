6139b97d3115196be5729d9bbaeab9df74ffee33
xulihang <xulihanghai@163.com>
2023-09-23 18:42:01 +0800

    add the link to open the local translator

--- ImageTrans/popup.js
+++ ImageTrans/popup.js
@@ -8,6 +8,17 @@ let help = document.getElementsByClassName('help')[0];
 document.getElementsByClassName('options')[0].addEventListener("click",function(){
 	chrome.runtime.openOptionsPage();
 });
+document.getElementsByClassName('local')[0].addEventListener("click",function(){
+	chrome.storage.sync.get({
+			serverURL: "https://local.basiccat.org:51043"
+	}, async function(items) {
+		  var URL = "https://local.basiccat.org:51043";
+			if (items.serverURL) {
+					URL = items.serverURL;
+			}
+			window.open(URL,"_blank");
+	});
+});
 
 btn.onclick = function() {
 	connect(false,"getsrconly");
