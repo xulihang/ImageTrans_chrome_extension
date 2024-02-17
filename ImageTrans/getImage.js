0d73b2ce5da0289a9cd32a140a19b18616b07f3b
xulihang <xulihanghai@163.com>
2024-02-17 11:43:25 +0800

    use image's natural width and height

--- ImageTrans/getImage.js
+++ ImageTrans/getImage.js
@@ -125,8 +125,8 @@ function getDataURLFromImg(img) {
     if (!canvas) {
         canvas = document.createElement("canvas");
     }
-    canvas.width = img.width;
-    canvas.height = img.height;
+    canvas.width = img.naturalWidth;
+    canvas.height = img.naturalHeight;
     const ctx = canvas.getContext('2d');
     ctx.drawImage(img, 0, 0);
     return canvas.toDataURL("image/jpeg");
