f009fb08eb710d5b755b91746a3d5b6e2632b421
xulihang <xulihanghai@163.com>
2024-12-10 17:59:07 +0800

    open online image translator if no local server is running

--- ImageTrans/popup.js
+++ ImageTrans/popup.js
@@ -10,13 +10,18 @@ document.getElementsByClassName('options')[0].addEventListener("click",function(
 });
 document.getElementsByClassName('local')[0].addEventListener("click",function(){
 	chrome.storage.sync.get({
-			serverURL: "https://local.basiccat.org:51043"
+		serverURL: "https://local.basiccat.org:51043"
 	}, async function(items) {
-		  var URL = "https://local.basiccat.org:51043";
-			if (items.serverURL) {
-					URL = items.serverURL;
-			}
-			window.open(URL,"_blank");
+		var URL = "https://local.basiccat.org:51043";
+		if (items.serverURL) {
+			URL = items.serverURL;
+		}
+		try {
+			await fetch(URL);
+		}catch (e) {
+			URL = "https://www.basiccat.org/online-image-translator";
+		} 
+		window.open(URL,"_blank");
 	});
 });
 
