3180e36a86e493c02474856cb1d1789fd3fe94b1
xulihang <xulihanghai@163.com>
2024-12-10 17:23:13 +0800

    add buttons to quickly set the url and add display name setting

--- ImageTrans/options.js
+++ ImageTrans/options.js
@@ -2,10 +2,12 @@ function save() {
   const URL = document.getElementById("serverURL").value;
   const pickingWay = document.getElementById("pickingWay").selectedOptions[0].value;
   const useCanvas = document.getElementById("useCanvas").checked;
+  const imagetransInstanceDisplayName = document.getElementById("imagetransInstanceInput").value;
   chrome.storage.sync.set({
     serverURL: URL,
     pickingWay: pickingWay,
-    useCanvas: useCanvas
+    useCanvas: useCanvas,
+    displayName: imagetransInstanceDisplayName
   }, function() {
     // Update status to let user know options were saved.
     alert("saved");
@@ -16,7 +18,8 @@ function load() {
   chrome.storage.sync.get({
     serverURL: 'https://local.basiccat.org:51043',
     pickingWay: '1',
-    useCanvas: true
+    useCanvas: true,
+    displayName: "",
   }, function(items) {
     if (items.serverURL) {
         document.getElementById("serverURL").value = items.serverURL;
@@ -33,6 +36,7 @@ function load() {
       document.getElementById("pickingWay").selectedIndex = 1;
     }
     document.getElementById("useCanvas").checked = items.useCanvas;
+    document.getElementById("imagetransInstanceInput").value = displayName;
   });
 }
 
@@ -40,5 +44,11 @@ window.onload = function (){
   load();
   document.getElementById("saveButton").addEventListener("click",function(){
     save();
+  })
+  document.getElementById("publicServerButton").addEventListener("click",function(){
+    document.getElementById("serverURL").value = "https://service.basiccat.org:51043";
+  })
+  document.getElementById("localServerButton").addEventListener("click",function(){
+    document.getElementById("serverURL").value = "https://local.basiccat.org:51043";
   })
 }
